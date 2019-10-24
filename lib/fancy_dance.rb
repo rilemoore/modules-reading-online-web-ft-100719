69387c9c4929582f9a2a8cb7c32338585a0fa85d
Riley Moore <rilemoore@gmail.com>
2019-10-24 00:16:05 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -16,12 +16,12 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
+  end
   
   
-  
-    module ClassMethods
+  module ClassMethods
     
-      def metadata
+    def metadata
         "This class produces objects that love to dance."
       end
     end
